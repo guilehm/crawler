fae86af79e6b94e1d803dc69ac710c6e62858754
guilherme <guile.hm@hotmail.com>
2019-02-03 15:09:58 -0200

    Setup swagger and redoc

--- crawler/urls.py
+++ crawler/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Feed",
+      default_version='v1',
+      description="Feed de notícias",
+      contact=openapi.Contact(email="guile.hm@hotmail.com"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
