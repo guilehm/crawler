3f0b7ce20cbde31b9969bf437cf48e805cb4cf37
guilherme <guile.hm@hotmail.com>
2019-02-03 16:30:55 -0200

    Create signup view, url and templates

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import redirect, render
+from rest_framework.authtoken.models import Token
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            Token.objects.create(user=user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/accounts/signup.html', {'form': form})
 
 
 def logout_view(request):
