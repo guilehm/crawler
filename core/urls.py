e31ab0aca123e8d3c8dcb12635c47c6e5bd3fc99
Guilherme Luis <guile.hm@hotmail.com>
2019-02-04 21:00:26 -0200

    Add url settings for media files

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from core import views
@@ -11,3 +13,6 @@ urlpatterns = [
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
