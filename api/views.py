1322fa27297c30aa7ff1781ef335a1b2ab3accb2
Guilherme Luis <guile.hm@hotmail.com>
2019-02-04 23:29:52 -0200

    Create feed detail save view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+import json
+import tempfile
+
+from django.core.files import File
+from django.shortcuts import redirect
 from requests.exceptions import RequestException
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -6,6 +11,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
+from core.models import DataFile
 from utils import create_feed
 
 
@@ -42,3 +48,29 @@ class FeedDetailAllowAnyView(APIView):
             data=feed_data,
             status=status_code,
         )
+
+
+class FeedDetailSaveView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (JSONRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        feed_data = None
+        try:
+            feed_data = create_feed()
+        except RequestException:
+            status_code = HTTP_400_BAD_REQUEST
+            return Response(
+                data=feed_data,
+                status=status_code,
+            )
+
+        with tempfile.TemporaryFile(mode='w+') as file:
+            file.write(
+                json.dumps(feed_data, indent=4, separators=(',', ': '), ensure_ascii=False)
+            )
+            file.seek(0)
+            data_file = DataFile.objects.create(
+                file=File(file, name='feed.json')
+            )
+        return redirect('core:feed-detail', feed_id=data_file.id)
