342dfed37802dc49c2255724d932b50de6a6948d
Guilherme Luis <guile.hm@hotmail.com>
2019-02-06 00:23:38 -0200

    Remove postgres

--- crawler/settings.py
+++ crawler/settings.py
@@ -84,11 +84,8 @@ WSGI_APPLICATION = 'crawler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'HOST': 'db' if env('PYTHONBUFFERED', default=False) else 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
