9033e6a2d6407945f415b20747d42dc49eb79d88
Guilherme Luis <guile.hm@hotmail.com>
2019-02-06 00:11:37 -0200

    Set environ

--- crawler/settings.py
+++ crawler/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,9 +85,9 @@ WSGI_APPLICATION = 'crawler.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'mydb',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'db' if env('PYTHONBUFFERED', default=False) else 'localhost',
         'PORT': 5432,
     }
 }
